c4dd087ddb57ea9a6a739915eb855c00374727a3
Nex <nex@nex.sx>
2022-03-21 01:11:28 +0100

    Fixed copyright notice

--- utils/utils.go
+++ utils/utils.go
@@ -1,4 +1,4 @@
-// pcqf - PC Quick Forensics
+// androidqf - Android Quick Forensics
 // Copyright (c) 2021-2022 Claudio Guarnieri.
 // Use of this software is governed by the MVT License 1.1 that can be found at
 //   https://license.mvt.re/1.1/
